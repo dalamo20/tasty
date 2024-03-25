01ff6234ec1366ba26f4934624fb54bd1511f89e
dalamo20 <dalamo90@gmail.com>
2024-03-24 22:56:12 -0500

    added carousel (not finished), fixing hero and nav

---
+++ src/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".flickity-gallery").flickity({
+    // options
+    autoPlay: true,
+    cellAlign: "left",
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+  });
+});

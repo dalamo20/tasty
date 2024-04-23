836eccd2960c34bd27d9c803d9438dc190a8a86b
dalamo20 <dalamo90@gmail.com>
2024-04-22 22:51:36 -0500

    carousel for reels added

--- public/src/app.js
+++ public/src/app.js
@@ -32,13 +32,20 @@ const doLogout = function (e) {
 };
 // END OF AUTH
 
-$(document).ready(function () {
-  $(".flickity-gallery").flickity({
-    // options
-    autoPlay: true,
-    cellAlign: "left",
-    contain: true,
-    freeScroll: true,
-    wrapAround: true,
+// COOKING REELS CAROUSEL
+const reelContainers = [...document.querySelectorAll(".reel-container")];
+const nxtBtn = [...document.querySelectorAll(".next")];
+const preBtn = [...document.querySelectorAll(".previous")];
+
+reelContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
   });
 });
